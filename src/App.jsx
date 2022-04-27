1a1877fd1d9ce01c1b843ba442a8e56e04bc9ec1
Luis Carrera <luiscarreramv@gmail.com>
2022-04-27 03:25:09 -0600

    Terminando la clase 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React, { useRef } from 'react'
-import FormNoControlado from './components/FormNoControlado'
+import Formulario from "./components/Formulario"
 
 const App = () => {
 
   return (
     <div className='container pt-4'>
       <h1>Formulario</h1>
-      <FormNoControlado />
+      <Formulario />
     </div>
   )
 }
